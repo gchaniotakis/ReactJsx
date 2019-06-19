433a8127111eb6c5b3b735e33fc4cf9963a0b616
George Chaniotakis <haniotinho@gmail.com>
2019-06-19 17:07:31 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
 // Create a React component
 const App = () => {
+    const buttonText = 'Click Me!';
+    const style = {backgroundColor: 'blue', color:'white'};
     return( 
     <div>
-    <label class="label"for="name">Enter name:</label>
+    <label className="label"for="name">Enter name:</label>
     <input id="name" type="text"/>
-    <button style={{backgroundColor: 'blue', color:'white'}}>Submit</button>
+    <button style={style}>{buttonText}</button>
     </div>
     );
 };
