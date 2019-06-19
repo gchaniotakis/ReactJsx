415624f583ad3cf829c8b95135f066aa1e6248bb
George Chaniotakis <haniotinho@gmail.com>
2019-06-19 03:11:41 +0300

    initialising index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Imort the React and ReactDOM ilbraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+
+
+// Take the React component and show it on the screen
